3b353a850e50f2ca3d9705014074aa159173c937
Cloud User <ec2-user@ip-172-31-8-137.ap-south-1.compute.internal>
2022-02-24 16:31:05 +0000

    updatedDBConnect.java file

---
+++ DBConnect.java
@@ -0,0 +1,5 @@
+hello git 
+git is a distributed version control system (vcm)
+	git is a software that runs locally 
+git commit -a -m "updatedDBConnect.java file"
+if we use -a alon with commit command ,no need to use git add command.
