fb1094a01e6a71250332c959866b34d29f089350
Kodeanusha <kodeanu@gmail.com>
2022-02-24 17:29:55 +0000

    Revert "hai commit" This reverts commit e0abc5a0b77213cc3e9194a9cc3052b96f9042b7.

--- DBConnect.java
+++ DBConnect.java
@@ -3,4 +3,3 @@ git is a distributed version control system (vcm)
 	git is a software that runs locally 
 git commit -a -m "updatedDBConnect.java file"
 if we use -a alon with commit command ,no need to use git add command.
-hai
