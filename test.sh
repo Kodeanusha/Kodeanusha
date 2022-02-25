51e895eb56be4e7db8d8a3d74f7dd866bc4d37c0
Kodeanusha <92987513+Kodeanusha@users.noreply.github.com>
2022-02-25 13:08:10 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,3 +2,5 @@ hello guys
 hello guys 
 git branch command is used to create,delete and list the branches in local repo.
 by using git merge we can integrate one branch code to another.
+git pull =git fetch + fit merge
+some updated code is there in remote repo to get that code to local repo it we will use git fetch
